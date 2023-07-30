f7c947fe084362685174d49dde812ed4dee80223
Mario Gutierrez <gutierrez.m101587@gmail.com>
2023-07-30 14:01:18 +0200

    remove unnecessary mock data in Main class

--- backend/src/main/java/space/ml_tech/SpringbootFullstackAppApplication.java
+++ backend/src/main/java/space/ml_tech/SpringbootFullstackAppApplication.java
@@ -1,38 +1,11 @@
 package space.ml_tech;
 
-import space.ml_tech.customer.Customer;
-import space.ml_tech.customer.CustomerRepository;
-import com.github.javafaker.Faker;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.util.Random;
 
 @SpringBootApplication
 public class SpringbootFullstackAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootFullstackAppApplication.class, args);
-	}
-
-	@Bean
-	CommandLineRunner runner (CustomerRepository customerRepository) {
-		return args -> {
-
-			var faker = new Faker();
-			var name = faker.name();
-			var email = faker.internet().emailAddress();
-
-			var randomAge = new Random();
-
-			Customer customer = Customer.builder()
-					.name(name.fullName())
-					.email(email)
-					.age(randomAge.nextInt(16, 99))
-					.build();
-
-			customerRepository.save(customer);
-		};
 	}
 }
